4d8e61e4ea6bd4af51f5fbcff7a282974598ab8f
gurielmp <gurielamp@gmail.com>
2019-03-24 13:10:48 +0700

    add responsive grid material ui

--- app/src/components/Segment1.js
+++ app/src/components/Segment1.js
@@ -4,31 +4,31 @@ import Paper from '@material-ui/core/Paper'
 
 const Segment1 = () => {
  return(
-  <div>
-      <Grid container spacing={24}>
-        <Grid item xs={12}>
-          <Paper>xs=12</Paper>
-        </Grid>
-        <Grid item xs={6}>
-          <Paper>xs=6</Paper>
-        </Grid>
-        <Grid item xs={6}>
-          <Paper>xs=6</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper>xs=3</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper>xs=3</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper>xs=3</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper>xs=3</Paper>
-        </Grid>
-      </Grid>
-    </div>
+  <div className="test">
+   <Grid container spacing={24}>
+     <Grid item xs={12}>
+       <Paper>xs=12</Paper>
+     </Grid>
+     <Grid item xs={12} sm={6}>
+       <Paper>xs=12 / sm=6</Paper>
+     </Grid>
+     <Grid item xs={12} sm={6}>
+       <Paper>xs=12 / sm=6</Paper>
+     </Grid>
+     <Grid item xs={12} sm={3}>
+       <Paper>xs=12 / sm=3</Paper>
+     </Grid>
+     <Grid item xs={12} sm={3}>
+       <Paper>xs=12 / sm=3</Paper>
+     </Grid>
+     <Grid item xs={12} sm={3}>
+       <Paper>xs=12 / sm=3</Paper>
+     </Grid>
+     <Grid item xs={12} sm={3}>
+       <Paper>xs=12 / sm=3</Paper>
+     </Grid>
+   </Grid>
+  </div>
  )
 }
 
