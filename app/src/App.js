e781de233a6a2e73abed8d1dfa338f6bf7ba8f16
gurielmp <gurielamp@gmail.com>
2019-03-24 12:02:26 +0700

    add navbar component from material ui

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import Button from '@material-ui/core/Button';
+import NavBar from './components/NavBar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer">
-            Learn React
-          </a>
-          <Button variant="contained" color="primary">This is a button from Material UI</Button>
-        </header>
+      <div>
+       <NavBar />
       </div>
     );
   }
